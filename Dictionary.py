82c08426a7be83307b2153497e7e598ffc2e4820
Abbas Ahmad <aa6057467@gmail.com>
2026-03-30 14:03:28 +0500

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,17 @@
+
+
+#Dictionary
+
+student1 = { "name" : "Abbas Ahmad",
+"Father name" : "Siraj Muhammad",
+"age" : 22,
+"married" : True,
+"CGPA" : 3.5,
+'Friends' : ["a", "b"],
+"Address" : {
+    "village" : "Showwand",
+    "tehsil" : "Razzar",
+    "District" : "Swabi"}
+}
+print(student1)
+print(student1["Address"]["village"])
