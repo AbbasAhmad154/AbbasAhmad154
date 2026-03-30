9e8ba75dc82b5eb69c71339d9c4772a1932b96cd
Abbas Ahmad <aa6057467@gmail.com>
2026-03-30 13:59:05 +0500

    Add files via upload

---
+++ Day 1 project Football team name (1).py
@@ -0,0 +1,5 @@
+print("Assalamualaikum")
+print("Welcome to the football team name generator.")
+city=input("Which city did you grow up in? \n")
+pet=input("What is the name of a pet? \n")
+print("Your team name could be " +city+ " " + pet )
